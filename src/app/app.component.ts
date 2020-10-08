17615f81056ec37ecd8a55f0ff05f6dd4ff02bd4
psl <polischuk.post@gmail.com>
2020-10-08 19:24:58 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'magnetics';
+  private func(
+    reallyLongArg,
+    omgSoManyParameters,
+    IShouldRefactorThis,
+    isThereSeriouslyAnotherOne,
+  ): any {
+    console.log('rrrrr');
+  }
 }
